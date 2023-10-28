d77fe4abbd97722c4b569b99d94048413e711d22
wathika-eng <wathika02@gmail.com>
2023-10-28 10:44:18 +0300

    formatting

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 #!/usr/bin/env python3
-#py in env varibles
+"""
+python in the env variables
+"""
 
 import sys
+
 """
 module to import
 """
+
 
 def factors(file):
     """
     Factorizes the numbers from test00 and prints their factorizations
     """
     with open(file, "r") as nf:
+        """
+        open the file using py file handling
+        """
         for line in nf:
+            """
+            loop through the numbers
+            """
             num = int(line)
-            for x in range(2, int(num**0.5) + 1):
+            for x in range(2, int(num ** 0.5) + 1):
+                """
+                find the factorial
+                """
                 if num % x == 0:
+                    """
+                    stop when it gets to 0
+                    """
                     print(f"{num}={x}*{num//x}")
                     break
+
 
 if __name__ == "__main__":
     """
@@ -26,4 +43,7 @@ if __name__ == "__main__":
         print("Usage: python factors.py <input_file>")
         sys.exit(1)
     input_file = sys.argv[1]
+    """
+    take file as input
+    """
     factors(input_file)
